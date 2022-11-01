7c812ad6dc2ee4d70e0fa4681bb0767630d1d28b
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2022-11-01 15:58:09 +0530

    solved fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,26 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+
+    let num = 1;
+
+    while (num <= n) {
+
+        if (num % 3 === 0 && num % 5 === 0) {
+            console.log("fizzbuzz");
+        } else if (num % 5 === 0) {
+            console.log("buzz");
+        } else if (num % 3 === 0) {
+            console.log("fizz");
+        } else {
+            console.log(num);
+        }
+
+        num++;
+
+    }
+
+}
 
 module.exports = fizzBuzz;
