2c4d6e36696d307e989a5f4c262ffc5a30970196
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2022-11-01 12:07:05 +0530

    palindorme with diff solutions

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,47 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// Solution 1
+function palindrome(str) {
+    let reversedString = '';
+    for (let character of str) {
+        reversedString = character + reversedString;
+    }
+    return reversedString === str;
+}
+
+// Solution 2
+function palindrome(str) {
+    const reversedString = str.split('').reverse().join('');
+    return reversedString === str;
+}
+
+// Solution 3
+function palindrome(str) {
+    const reversedString = str.split('').reduce((prev, char) => {
+        return char + prev;
+    }, '');
+    return reversedString === str;
+}
+
+// Solution 4
+function palindrome(str) {
+    return str.split("").every((char, i)=> {
+        return char === str[str.length - 1 - i]
+    });
+};
+
+//Solution 5
+
+function palindrome(str) {
+    for (let index = 0; index < str.length/2; index++) {
+        const first = str[index];
+        const last = str[str.length-1-index]
+        if(first !== last) return false;
+    }
+    return true;
+}
+
+
 
 module.exports = palindrome;
