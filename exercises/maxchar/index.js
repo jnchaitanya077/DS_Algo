f7ff3f5050173a391ba9c4f1115f7a74deaae5c2
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2022-11-01 15:32:39 +0530

    solved maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    let map = {};
+    let maxRepated = null;
+
+    for (const char of str) {
+        map[char] = map[char] + 1 || 1;
+    }
+
+    let keys = Object.keys(map);
+
+    for (let index = 0; index < keys.length; index++) {
+        
+        if(index === 0){
+            maxRepated = keys[index];
+            continue;
+        }
+        if(map[keys[index]] > map[maxRepated]) {
+            maxRepated = keys[index];
+        }
+        
+    }
+
+    return maxRepated;
+}
 
 module.exports = maxChar;
