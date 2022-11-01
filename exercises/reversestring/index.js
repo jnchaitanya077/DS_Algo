b89e8f7e2660323a47e847f707ff7b59dd1290b5
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2022-11-01 07:37:22 +0530

    added solution for reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+
+// Solution 1
+function reverse(str) {
+  let rev = "";
+  // Itetrate through string from last and concat to the new empty string.
+  for (let character of str) {
+    rev = character + rev;
+  }
+  return rev;
+}
+
+// Solution 2
+
+function reverse(str) {
+    return str.split("").reverse().join("");
+  }
+
+// Solution 3
+function reverse(str) {
+    return str.split('').reduce((prevConcatString, character)=>{
+        return character + prevConcatString ;
+
+    }, '');  
+};
 
 module.exports = reverse;
