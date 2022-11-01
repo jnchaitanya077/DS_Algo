eb29abb889b3b1729499c0f6efc95c2ec21e6e97
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2022-11-01 13:24:02 +0530

    solved reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,34 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// Solution 1
+function reverseInt(n) {
+    let sign = Math.sign(n);
+
+    if (sign < 0) {
+        n = -1 * n;
+    }
+
+    let rev = parseInt(n.toString().split('').reduce((p, c) => c + p, ''));
+    return rev * sign;
+}
+
+// Solution 2
+function reverseInt(n) {
+    let rev = 0;
+    let sign = Math.sign(n);
+
+    if (sign < 0) {
+        n = -1 * n;
+    }
+
+    while (n > 0) {
+        let rem = n % 10;
+        rev = (rev * 10) + rem;
+        n = Math.floor(n / 10);
+    }
+    return rev * sign;
+
+}
 
 module.exports = reverseInt;
