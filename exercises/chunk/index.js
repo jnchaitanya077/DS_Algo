8f310b69568d9eadf96f23c19767e481a7652fd6
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2022-11-01 20:35:38 +0530

    solved chuck problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,54 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// Solution 1
+function chunk(array, size) {
+
+    let count = 0;
+    let mainArray = [];
+    let subArray = [];
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        subArray.push(element);
+        count++;
+
+        if (count === size || index === array.length - 1) {
+            mainArray.push(subArray);
+            subArray = [];
+            count = 0;
+        }
+    }
+    return mainArray;
+}
+
+// Solution 2
+function chunk(array, size) {
+
+    let mainArray = [];
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        let last = mainArray[mainArray.length - 1];
+
+        if(!last || size === last.length) {
+            mainArray.push([element]);
+        } else{
+            last.push(element);
+        }
+    }
+    return mainArray;
+}
+
+// Solution 3
+function chunk(array, size) {
+
+    let mainArray = [];
+
+    for (let i = 0; i < array.length; i = i + size) {
+        mainArray.push(array.slice(i, i + size));
+    }
+    return mainArray;
+}
 
 module.exports = chunk;
