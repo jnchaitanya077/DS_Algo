47d1dcec851274a32b47902c56747667bd4c37e9
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2022-11-04 11:02:32 +0530

    matrix problem solved

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,52 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let counter = 1;
+
+    let startRow = 0;
+    let endRow = n-1;
+    let startCol = 0;
+    let endCol = n-1;
+
+    let spiral = [];
+
+    for (let index = 0; index < n; index++) {
+        spiral.push([]);
+    };
+
+    while (startRow <= endRow && startCol <= endCol) {
+        
+        // for first row
+        for (let i = startCol; i <= endCol; i++) {
+            spiral[startRow][i] = counter;
+            counter++; 
+        };
+        startRow++;
+
+        // last col
+        for (let j = startRow; j <= endRow; j++) {
+            spiral[j][endCol] = counter;
+            counter++;
+        };
+        console.log(spiral);
+        endCol--;
+
+        //last row
+        for (let k = endCol; k >= startCol; k--) {
+            spiral[endRow][k] = counter;
+            counter++;
+        };
+        endRow--;
+
+        // middle colums
+        for (let l = endRow; l >= startRow; l--) {
+            spiral[l][startCol]= counter;
+            counter++;
+        }
+        startCol++;
+    }
+    return spiral;
+}
 
 module.exports = matrix;
