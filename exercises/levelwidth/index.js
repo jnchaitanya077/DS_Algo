c08c70cd083f9560adcc85e3b35f7e7c7f48f9b2
jnchaitanya077 <chaitu.krish4@msitprogram.net>
2022-11-11 10:26:33 +0530

    levelwidth completed

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,35 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+const Node = require("./node");
+
+function levelWidth(root) {
+    let queue = [root, "s"];
+    let counters = [0];
+    let level = 0;
+
+    while (queue.length > 1) {
+        let element = queue.shift();
+        if (element === "s") {
+            queue.push(element);
+            level++;
+        } else {
+            counters[level] = counters[level] + 1 || 1;
+            queue.push(...element.children);
+        }
+    }
+    return counters;
+}
+
+let node = new Node(1);
+
+node.add(2);
+node.add(3);
+node.add(4);
+
+node.children[0].add(5);
+node.children[2].add(6);
+
+levelWidth(node);
 
 module.exports = levelWidth;
